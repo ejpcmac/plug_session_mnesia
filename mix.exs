ee73efeb620a16d4be999bfc31b7ccb8e667504e
Jean-Philippe Cugnet <jean-philippe@cugnet.eu>
2019-08-04 13:02:51 +0200

    Bump the version to 0.1.3-dev

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule PlugSessionMnesia.Mixfile do
   use Mix.Project
 
-  @version "0.1.2"
+  @version "0.1.3-dev"
   @repo_url "https://github.com/ejpcmac/plug_session_mnesia"
 
   def project do
